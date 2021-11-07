b79955fd97a6276b0f21bdaa5e0ca672cb52fe9f
ahmed-bs <medamed777@gmail.com>
2021-11-07 23:51:20 +0000

    first commit

---
+++ lib/classes/Depense.dart
@@ -0,0 +1,7 @@
+import 'package:mobile/classes/Transaction.dart';
+
+class Depense extends Transacition {
+
+  Depense(int idtrans,String heure,String day,int montant,String description)
+      : super(idtrans, heure, day ,montant ,description);
+}
