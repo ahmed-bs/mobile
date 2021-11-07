5c23b56a1b0db0eb576749962c16aad112e5bc4d
ilhembensalha <bensalhailhem@gmail.com>
2021-11-08 00:58:46 +0100

    entities categorie et sous-categorie

---
+++ lib/classes/Sous_categorie.dart
@@ -0,0 +1,7 @@
+import 'package:mobile/classes/Categorie.dart';
+
+class Sous_categorie extends Categorie {
+
+  Sous_categorie(int idcat, String nom, String description)
+      : super(idcat, nom,description);
+}
